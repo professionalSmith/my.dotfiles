ad500d318b6acb9f7dc30d9f9242581155be1761
David Smith <3581358+professionalSmith@users.noreply.github.com>
2025-12-30 14:20:05 -0500

    feat(work): add work shortcuts
    * added aliases for opening DirectEmployer's repositories
    * added ignored directory to stash files temporarily without stashing

---
+++ packages/work/.dotfiles/apps/work.sh
@@ -0,0 +1,8 @@
+# General aliases
+alias work='cd ~/work.repos'
+
+# Repository aliases
+alias gitops='cd ~/work.repos/gitops && code .'
+alias imports-service='cd ~/work.repos/imports-service && code .'
+alias my-jobs='cd ~/work.repos/my-jobs && code .'
+alias prompt-based-search='cd ~/work.repos/prompt-based-search && code .'
