9de1e2de60d543316c9243f9d53be99b8eec9535
David Smith <3581358+professionalSmith@users.noreply.github.com>
2025-12-30 00:27:21 -0500

    build(releases): automate release process
    * integrated commitizen for release management
    * configured commitizen for conventional commit linting
    * added hook ensuring commit messages are linted
    * added Make rule for automatically bumping version
    * added Make rule for manually incrementing major version
    * added Make rule for manually incrementing minor version
    * added Make rule for manually incrementing patch version
    * added Make rule for checking out main branch
    * added Make rule for generating CHANGELOG.md
    * added Make rule for generating version tag
    * added Make rule for validating commits warrant version bump
    * added Make rule for wrapping Make rules required for releases

---
+++ makefiles/release.mk
@@ -0,0 +1,47 @@
+.PHONY: all clean test
+
+TAG_LATEST := $$(git describe --tags $(git rev-list --tags --max-count=1))
+
+bump:
+	# Automatically determine the next version and create a new tag
+	cz bump --yes --changelog --annotated-tag --no-verify
+
+bump-check:
+	# Check what the next version is without making any changes
+	cz bump --yes --changelog --annotated-tag --no-verify --allow-no-commit --dry-run
+
+bump-major:
+	# Automatically determine the next version and create a new tag
+	cz bump --yes --changelog --annotated-tag --no-verify --increment major
+
+bump-minor:
+	# Automatically determine the next version and create a new tag
+	cz bump --yes --changelog --annotated-tag --no-verify --increment minor
+
+bump-patch:
+	# Automatically determine the next version and create a new tag
+	cz bump --yes --changelog --annotated-tag --no-verify --increment patch
+
+checkout-main-branch:
+	# Checkout the main branch
+	git checkout main
+
+generate-changelog:
+	# Generate or update the changelog
+	cz changelog
+
+push-changes:
+	# Push changes to the remote repository
+	git push origin main
+
+push-tag:
+	# Push the newly created tag to the remote repository
+	git push origin $(TAG_LATEST)
+
+release: checkout-main-branch bump push-changes push-tag
+	# Perform a full release: bump version, update changelog, and push tag
+	gh release create $(TAG_LATEST) --draft --generate-notes --latest --verify-tag
+
+validate-commits:
+	# Validate commit messages according to Conventional Commits
+	cz check
