7e00c98c11c5ee7fb2b86be65da5e04ba2b7eeaf
David Smith <3581358+professionalSmith@users.noreply.github.com>
2026-01-12 09:15:52 -0500

    fix(shell): fix zsh completions
    Installing zsh-completions brew package recommends the following change.
    
    * added recommendations from Homebrew install zsh-completions package.

--- packages/shell/.dotfiles/apps/zsh.sh
+++ packages/shell/.dotfiles/apps/zsh.sh
@@ -1,5 +1,11 @@
+# Enable command auto-completion
 zstyle ':completion:*:*:make:*' tag-order 'targets'
 autoload -U compinit && compinit
+if type brew &>/dev/null; then
+    FPATH=$(brew --prefix)/share/zsh-completions:$FPATH
+    autoload -Uz compinit
+    compinit
+fi
 
 # Enable direnv
 eval "$(direnv hook zsh)"
